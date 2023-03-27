d2e5699d445069858b8073c27f122905715c112c
Dildora <48212@edu.raseko.fi>
2023-03-27 11:38:14 +0300

    Perus loopi tehty

---
+++ game.py
@@ -0,0 +1,23 @@
+import pygame
+
+def main():
+    pygame.init()
+    
+    screen = pygame.display.set_mode((800,600))
+    clock = pygame.time.Clock()
+    
+    running = True
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((230, 230, 255))
+        
+        pygame.display.flip()
+        clock.tick(60) # Odota niin kauan, että ruudun päivitysnopeus on fps 
+               
+    pygame.quit()
+    
+if __name__ == "__main__":
+    main()
