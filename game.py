6e03943ab65e73a066cf3083ac28889acc23ffb9
Dildora <48212@edu.raseko.fi>
2023-03-27 12:19:23 +0300

    lisä
    lisä

--- game.py
+++ game.py
@@ -1,23 +1,41 @@
 import pygame
 
+
 def main():
-    pygame.init()
-    
-    screen = pygame.display.set_mode((800,600))
-    clock = pygame.time.Clock()
-    
-    running = True
-    
-    while running:
+    game = Game()
+    game.run()
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+        self.running = False
+
+    def run(self):
+        clock = pygame.time.Clock()
+        self.running = True
+        while self.running:
+            self.handle_events()
+            self.handle_game_logic()
+            self.update_screen()
+            # Odota niin kauan, että ruudun päivitysnopeus on 60fps
+            clock.tick(60)
+
+        pygame.quit()
+
+    def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-        screen.fill((230, 230, 255))
-        
+                self.running = False
+
+    def handle_game_logic(self):
+        pass
+
+    def update_screen(self):
+        self.screen.fill((230, 230, 255))
         pygame.display.flip()
-        clock.tick(60) # Odota niin kauan, että ruudun päivitysnopeus on fps 
-               
-    pygame.quit()
-    
+
+
 if __name__ == "__main__":
     main()
